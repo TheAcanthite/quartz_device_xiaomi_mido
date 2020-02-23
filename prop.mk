704fddc0d6702c44fec14532d8c67cdb24d591fa
Matthew Bouyack <mbouyack@google.com>
2020-02-23 22:38:03 +0530

    mido: Disable backpressure propagation in SurfaceFlinger

--- prop.mk
+++ prop.mk
@@ -98,6 +98,8 @@ ro.surface_flinger.use_smart_90_for_video=true
 ro.surface_flinger.set_display_power_timer_ms=10000
 ro.surface_flinger.set_touch_timer_ms=5000
 ro.surface_flinger.set_idle_timer_ms=9000
+debug.sf.hwc.canUseABC=1
+debug.sf.disable_backpressure=1
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
